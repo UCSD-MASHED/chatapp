0a4a5211e38d30b9b9534d4fe69a730797c18ee0
Chang Zhou <zhouchang29@gmail.com>
2020-11-03 17:32:53 -0800

    Test

--- src/__tests__/API.int.test.js
+++ src/__tests__/API.int.test.js
@@ -3,6 +3,6 @@ import App from '../App';
 
 test('write test name here', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn chatapp/i);
+  const linkElement = screen.getByText(/learn/i);
   expect(linkElement).toBeInTheDocument();
 });

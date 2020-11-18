543c4cbe08e74b75b43d593aec1fc9bab38911e0
yudian <yudian@aitc.ai>
2020-11-17 16:28:25 -0800

    revert

--- src/firebase.js
+++ src/firebase.js
@@ -5,38 +5,27 @@ import "firebase/firestore";
 
 let firebaseConfig;
 
-// if (process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test") {
-//   // we use beta app config
-//   firebaseConfig = {
-//     apiKey: process.env.REACT_APP_APIKEY_BETA,
-//     authDomain: process.env.REACT_APP_AUTHDOMAIN_BETA,
-//     databaseURL: process.env.REACT_APP_DATABASEURL_BETA,
-//     projectId: process.env.REACT_APP_PROJECTID_BETA,
-//     storageBucket: process.env.REACT_APP_STORAGEBUCKET_BETA,
-//     messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID_BETA,
-//     appId: process.env.REACT_APP_APPID_BETA,
-//   };
-// } else {
-//   firebaseConfig = {
-//     apiKey: process.env.REACT_APP_APIKEY,
-//     authDomain: process.env.REACT_APP_AUTHDOMAIN,
-//     databaseURL: process.env.REACT_APP_DATABASEURL,
-//     projectId: process.env.REACT_APP_PROJECTID,
-//     storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-//     messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-//     appId: process.env.REACT_APP_APPID,
-//   };
-// }
-
-firebaseConfig = {
-  apiKey: "AIzaSyClt_cw75ffjm45ehfsrcnpCSRm17p0Xyg",
-  authDomain: "twotenchatapp-beta-a41e8.firebaseapp.com",
-  databaseURL: "https://twotenchatapp-beta-a41e8.firebaseio.com",
-  projectId: "twotenchatapp-beta-a41e8",
-  storageBucket: "twotenchatapp-beta-a41e8.appspot.com",
-  messagingSenderId: "1056001987312",
-  appId: "1:1056001987312:web:c0e59a9e51560b70126835",
-};
-console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test") {
+  // we use beta app config
+  firebaseConfig = {
+    apiKey: process.env.REACT_APP_APIKEY_BETA,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN_BETA,
+    databaseURL: process.env.REACT_APP_DATABASEURL_BETA,
+    projectId: process.env.REACT_APP_PROJECTID_BETA,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET_BETA,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID_BETA,
+    appId: process.env.REACT_APP_APPID_BETA,
+  };
+} else {
+  firebaseConfig = {
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASEURL,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_APPID,
+  };
+}
 
 firebase.initializeApp(firebaseConfig);

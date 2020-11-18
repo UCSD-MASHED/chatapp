d1a23c6bfce13de99ac8ad65e14f4361998d40d1
yudian <yudian@aitc.ai>
2020-11-17 20:30:03 -0800

    test generate .env.dev file

--- src/firebase.js
+++ src/firebase.js
@@ -6,29 +6,38 @@ import "firebase/firestore";
 let firebaseConfig;
 
 console.log(process.env.NODE_ENV);
-console.log(process.env.REACT_APP_FIREBASE_CONFIG);
-if (process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test") {
-  // we use beta app config
-  firebaseConfig = {
-    apiKey: process.env.REACT_APP_APIKEY_BETA,
-    authDomain: process.env.REACT_APP_AUTHDOMAIN_BETA,
-    databaseURL: process.env.REACT_APP_DATABASEURL_BETA,
-    projectId: process.env.REACT_APP_PROJECTID_BETA,
-    storageBucket: process.env.REACT_APP_STORAGEBUCKET_BETA,
-    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID_BETA,
-    appId: process.env.REACT_APP_APPID_BETA,
-  };
-} else {
-  firebaseConfig = {
-    apiKey: process.env.REACT_APP_APIKEY,
-    authDomain: process.env.REACT_APP_AUTHDOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASEURL,
-    projectId: process.env.REACT_APP_PROJECTID,
-    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-    appId: process.env.REACT_APP_APPID,
-  };
-}
+console.log(process.env.REACT_APP_APIKEY);
+// if (process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test") {
+//   // we use beta app config
+//   firebaseConfig = {
+//     apiKey: process.env.REACT_APP_APIKEY,
+//     authDomain: process.env.REACT_APP_AUTHDOMAIN,
+//     databaseURL: process.env.REACT_APP_DATABASEURL,
+//     projectId: process.env.REACT_APP_PROJECTID,
+//     storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+//     messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+//     appId: process.env.REACT_APP_APPID,
+//   };
+// } else {
+//   firebaseConfig = {
+//     apiKey: process.env.REACT_APP_APIKEY,
+//     authDomain: process.env.REACT_APP_AUTHDOMAIN,
+//     databaseURL: process.env.REACT_APP_DATABASEURL,
+//     projectId: process.env.REACT_APP_PROJECTID,
+//     storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+//     messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+//     appId: process.env.REACT_APP_APPID,
+//   };
+// }
+firebaseConfig = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASEURL,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+};
 console.log(firebaseConfig);
 
 firebase.initializeApp(firebaseConfig);

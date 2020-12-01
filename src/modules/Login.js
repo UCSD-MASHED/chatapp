ec615306d6f48c72d8e8ada7c36d1ea58ca09e8e
yudian <yudian@aitc.ai>
2020-11-30 17:01:22 -0800

    format jsdoc, modify jsdoc config

--- src/modules/Login.js
+++ src/modules/Login.js
@@ -3,22 +3,24 @@ import firebase from "firebase/app";
 import { withRouter } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 
+/**
+ * This is the Login Component
+ */
 class Login extends React.Component {
   constructor(props) {
     super(props);
-
     this.handleGoogleSignIn = this.handleGoogleSignIn.bind(this);
   }
 
+  /**
+   * Handles Sign in button click, opens a pop up to google sign in page
+   * Once sign in is completed, we will get back the google user object.
+   * We will use the uid to distinguish each user. If the user is new,
+   * we will take them to CreateUser to ask them fill in a unique username,
+   * else we will take them to Chat.
+   * @param {Object} event - An Event Object
+   */
   handleGoogleSignIn(event) {
-    /*
-     * Handles Sign in button click, opens a pop up to google sign in page
-     * Once sign in is completed, we will get back the google user object.
-     * We will use the uid to distinguish each user. If the user is new,
-     * we will take them to CreateUser to ask them fill in a unique username,
-     * else we will take them to Chat.
-     * @param {Object} event - An Event Object
-     */
     event.preventDefault();
     var googleProvider = new firebase.auth.GoogleAuthProvider();
     firebase
@@ -52,16 +54,16 @@ class Login extends React.Component {
       });
   }
 
+  /**
+   * Get the user from googleUser
+   * @param {Object} googleUser - The google user to be found in database
+   * @param {string} googleUser.uid - The unique id of the google user
+   * @param {string} googleUser.displayName - The displayed name of the
+   *     google user
+   * @return {(user|undefined)} A user object if googleUser.uid is unique
+   *     in database; otherwise return undefined
+   */
   async getUser(googleUser) {
-    /*
-     * Get the user from googleUser
-     * @param {Object} googleUser - The google user to be found in database
-     * @param {string} googleUser.uid - The unique id of the google user
-     * @param {string} googleUser.displayName - The displayed name of the
-     *     google user
-     * @return {(user|undefined)} A user object if googleUser.uid is unique
-     *     in database; otherwise return undefined
-     */
     var res = await firebase
       .firestore()
       .collection("users")

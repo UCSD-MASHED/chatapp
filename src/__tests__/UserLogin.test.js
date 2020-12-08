1af0985ce0b59a586a95b5cea4b1513f5b5914e8
Edward Chen <edwjchen@gmail.com>
2020-12-08 14:25:37 -0800

    updated test case

--- src/__tests__/UserLogin.test.js
+++ src/__tests__/UserLogin.test.js
@@ -40,7 +40,7 @@ beforeEach(() => {
 });
 
 test("Login page elements", () => {
-  const landingText = screen.getByText("TaterTalk");
+  const landingText = screen.getByText("TaterTalks");
   expect(landingText).toBeInTheDocument();
   const button = screen.getByText("Sign In");
   expect(button).toBeInTheDocument();

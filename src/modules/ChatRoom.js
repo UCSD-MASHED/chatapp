705837ebbfbc24c5539c8409a1732886980f47f8
Chang Zhou <zhouchang29@gmail.com>
2020-12-08 21:46:38 -0800

    Separate enter svg file, change chat message window size

--- src/modules/ChatRoom.js
+++ src/modules/ChatRoom.js
@@ -4,6 +4,7 @@ import { withRouter } from "react-router-dom";
 import ChatMessage from "./ChatMessage";
 import User from "./User";
 import Loading from "./Loading";
+import { ReactComponent as EnterButton } from './enter.svg'
 
 /**
  * This is the ChatRoom Component
@@ -382,19 +383,7 @@ class ChatRoom extends React.Component {
                     className="btn btn-success btn-block"
                     title="Send"
                   >
-                    <svg
-                      width="1em"
-                      height="1em"
-                      viewBox="0 0 16 16"
-                      className="bi bi-arrow-return-left"
-                      fill="currentColor"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5z"
-                      />
-                    </svg>
+                    <EnterButton />
                   </button>
                 </div>
               </div>

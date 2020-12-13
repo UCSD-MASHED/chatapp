223f9978d2ab0ddb4c06fc6e9e0c041fc365a965
yudian <yudian@aitc.ai>
2020-12-12 16:21:18 -0800

    experiment with jsdoc type definition

---
+++ src/modules/Types.js
@@ -0,0 +1,8 @@
+/**
+ * @typedef User
+ * @type {Object}
+ * @property {string} userId - the uid of the GoogleUser
+ * @property {string} username - the username of the user
+ * @property {list} roomIds - a list of ids of the rooms the user participates in
+ * @property {boolean} online - if the user is online
+ */

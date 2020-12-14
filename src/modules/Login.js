2e0d82c18294380f04274f44e35388b0c8e02dfc
Edward Chen <edwjchen@gmail.com>
2020-12-13 23:07:08 -0800

    Added tatertalk icon

--- src/modules/Login.js
+++ src/modules/Login.js
@@ -121,35 +121,43 @@ class Login extends React.Component {
     return this.state.loading ? (
       <Loading />
     ) : (
-      <div className="auth-wrapper">
-        <div className="signin-wrapper">
-          <span className="landing-left-wrapper">
-            <div className="landing-text">
-              <h1>TaterTalk</h1>
-              <br></br>
-              <p>Presented by Team Mashed</p>
-              <br></br>
-              <button
-                style={{ padding: "1.5vh 8vw", fontSize: "1.5rem" }}
-                onClick={this.handleGoogleSignIn}
-                className="btn btn-primary btn-sm"
-              >
-                Sign In
+        <div className="auth-wrapper">
+          <div className="signin-wrapper">
+            <span className="landing-left-wrapper">
+              <div className="landing-text">
+                <div>
+                  <img
+                    className="tatertalk-login d-none d-lg-inline"
+                    alt="icon"
+                    src={process.env.PUBLIC_URL + "/tatertalk_icon.png"}
+                  />
+                  <span>
+                    <h1 style={{ display: "inline" }}>TaterTalk</h1>
+                    <br></br>
+                    <p>Presented by Team Mashed</p>
+                  </span>
+                </div>
+                <button
+                  style={{ padding: "1.5vh 8vw", fontSize: "1.5rem" }}
+                  onClick={this.handleGoogleSignIn}
+                  className="btn btn-primary btn-sm"
+                >
+                  Sign In
               </button>
-            </div>
-          </span>
-          <span className="landing-right-wrapper">
-            <img
-              className="landing-img d-none d-md-block"
-              alt="illustration"
-              src={process.env.PUBLIC_URL + "/landing_illustration_base.png"}
-            />
-            <Landing imgState={this.state.imgState} />
-          </span>
+              </div>
+            </span>
+            <span className="landing-right-wrapper">
+              <img
+                className="landing-img d-none d-md-block"
+                alt="illustration"
+                src={process.env.PUBLIC_URL + "/landing_illustration_base.png"}
+              />
+              <Landing imgState={this.state.imgState} />
+            </span>
+          </div>
+          <ToastContainer />
         </div>
-        <ToastContainer />
-      </div>
-    );
+      );
   }
 }
 
